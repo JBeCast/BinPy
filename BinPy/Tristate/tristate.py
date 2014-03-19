497f4c0edaa3d3b1348c5b38ae36f092e20c6292
JBC <jbcsound@gmail.com>
2014-03-19 12:41:51 -0300

    Implemented tristate_logic function. Changed tri-state buffer to a function that returns a Gate instance.

--- BinPy/Tristate/tristate.py
+++ BinPy/Tristate/tristate.py
@@ -1,16 +1,16 @@
 from BinPy.Gates import *
 
-class Buffer(Gate):
-    def __init__(self, *args):
-        Gate.__init__(self, 2, 2, *args)
+def tristate_logic(in_states):
+    if in_states[1] == 0:
+        return 2
+    elif in_states[1] == 1:
+        return in_states[0]
+    else:
+        return 3
 
-    def _calc_output(self, in_states):
-        if self.inputs[1].state == 0:
-            return 2
-        elif self.inputs[1].state == 1:
-            return self.inputs[0].state
-        else:
-            return 3
+def Buffer(*args):
+    return Gate(tristate_logic, 2, 2, *args)
+
 
 # Useful to implement output enable in ICs
 class Bus(object):
